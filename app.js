2c74f7c45439472d671c078ece5a502d6be371e1
Chirag <malhotrachirag2838@gmail.com>
2018-04-07 11:18:51 +0530

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./api/data/db.js');
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -5,7 +6,13 @@ var bodyParser = require('body-parser');
 
 app.set('port', 3000);
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(function(req, res, next){
+
+	console.log(req.method + " " + req.url);
+	next();
+})
+
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.use(bodyParser.urlencoded({extended : false}));
 
@@ -13,4 +20,4 @@ var server = app.listen(app.get('port'), function(){
 
 	var port = server.address().port;
 	console.log('server started at port no ' + port);
-})
+});
